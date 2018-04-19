0b083cad4900fab66ce296e2c1498b8ea206c310
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-19 17:38:20 +0200

    feat: add redux boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import {getStudents, getTutors, getConnections} from './actions';
+
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <div id="app"></div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    students: state.students,
+    tutors: state.tutors,
+    connections: state.connections
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getStudents: (students) => dispatch(getStudents(students)),
+  getTutors: (tutors) => dispatch(getTutors(tutors)),
+  getConnections: (connections) => dispatch(getConnections(connections))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/actions.js
@@ -0,0 +1,14 @@
+export const getTutors = (tutors) => ({
+  type: 'getTutors',
+  tutors
+});
+
+export const getStudents = (students) => ({
+  type: 'getStudents',
+  students
+});
+
+export const getConnections = (connections) => ({
+  type: 'getConnections',
+  connections
+});
