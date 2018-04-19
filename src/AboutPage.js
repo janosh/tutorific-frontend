24d82db769722fdc2bf680cc37694d745b0e884a
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-19 17:39:11 +0200

    feat: add react router dom and route 3 pages

---
+++ src/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class AboutPage extends React.Component {
+  render() {
+    return (
+      <div className="about-page">
+        <h1>This is the about page.</h1>
+      </div>
+    );
+  }
+}
---
+++ src/SignUpPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class SignUpPage extends React.Component {
+  render() {
+    return (
+      <div className="signup-page">
+        <h1>This is the sign up page.</h1>
+      </div>
+    );
+  }
+}
