79a8c96a432cfa78d0136b4deea0ab2907d47443
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-19 23:15:17 +0200

    feat: add reducers for tutors, students, appState

--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,29 @@
 import {combineReducers} from 'redux';
 
-const reducers = combineReducers({
+const tutors = (state = [], action) => {
+  if (action.type === 'getTutors') return action.tutors;
+  return state;
+};
 
+const students = (state = [], action) => {
+  if (action.type === 'getStudents') return action.students;
+  return state;
+};
+
+const appState = (state = [], action) => {
+  switch (action.type) {
+  case 'changePersonType':
+    return {
+      ...state,
+      personType: action.personType
+    }
+  default:
+    return state;
+  }
+};
+
+export default combineReducers({
+  tutors,
+  students,
+  appState
 });
-
-export default reducers;
-
