9a0825087c9cff6bb226130c398d09858b70cd32
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-20 18:30:26 +0200

    style: highlight active person list

--- src/components/PersonListItem.js
+++ src/components/PersonListItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './PersonListItem.css';
 
 export default function PersonListItem({person}) {
   return (
     <div className="person-list-item">
       <h3>{person.firstname} {person.lastname}</h3>
       <p className="status">status: {person.status}</p>
+      <p className="status">email: {person.email}</p>
+      <p className="status">subjects: {person.subjects.map(subject => subject.name).join(', ')}</p>
     </div>
   );
 }
