f80384f98f49c1ddab8d97e71736d23a68199f3b
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-20 22:18:49 +0200

    refactor: localize fetch requests from app to landing page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 import LandingPage from './pages/LandingPage';
 import AboutPage from './pages/AboutPage';
 import SignUpPage from './pages/SignUpPage';
-import {getStudents, getTutors, getConnections} from './actions';
 
 import './App.css';
 
-class App extends React.Component {
-
-  componentDidMount() {
-    fetch('http://localhost:3000/tutors')
-    .then(res => res.json())
-    .then(tutors => {
-      this.props.getTutors(tutors);
-    });
-
-    fetch('http://localhost:3000/students')
-    .then(res => res.json())
-    .then(students => {
-      this.props.getStudents(students);
-    });
-  }
+export default class App extends React.Component {
 
   render() {
     return (
@@ -55,19 +39,3 @@ class App extends React.Component {
     );
   }
 }
-
-const mapStateToProps = (state) => {
-  return {
-    students: state.students,
-    tutors: state.tutors,
-    connections: state.connections
-  };
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  getStudents: (students) => dispatch(getStudents(students)),
-  getTutors: (tutors) => dispatch(getTutors(tutors)),
-  getConnections: (connections) => dispatch(getConnections(connections))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
+import {getStudents, getTutors, getConnections} from '../actions';
 import PersonList from '../components/PersonList';
 import MapContainer from '../components/Map';
 import './LandingPage.css';
 
-export default class LandingPage extends React.Component {
+class LandingPage extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/tutors')
+    .then(res => res.json())
+    .then(tutors => {
+      this.props.getTutors(tutors);
+    });
+
+    fetch('http://localhost:3000/students')
+    .then(res => res.json())
+    .then(students => {
+      this.props.getStudents(students);
+    });
+  }
+
   render() {
     return (
       <div className="landing-page">
@@ -14,3 +31,19 @@ export default class LandingPage extends React.Component {
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    students: state.students,
+    tutors: state.tutors,
+    connections: state.connections
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getStudents: (students) => dispatch(getStudents(students)),
+  getTutors: (tutors) => dispatch(getTutors(tutors)),
+  getConnections: (connections) => dispatch(getConnections(connections))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LandingPage);
