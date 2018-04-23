277092c9dbb601e2d48a406bdedfeabac0a147f8
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-23 13:00:10 +0200

    feat: add LocationSearch component

---
+++ src/components/LocationSearch.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { compose, withProps, lifecycle } from 'recompose';
+import { withScriptjs } from 'react-google-maps';
+import { StandaloneSearchBox } from 'react-google-maps/lib/components/places/StandaloneSearchBox';
+import config from '../config';
+
+const LocationSearch = compose(
+  withProps({
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${config.googleMapsApiKey}&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div/>,
+  }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {}
+
+      this.setState({
+        places: [],
+        onSearchBoxMounted: ref => {
+          refs.searchBox = ref;
+        },
+        onPlacesChanged: () => {
+          const places = refs.searchBox.getPlaces();
+
+          this.setState({
+            places,
+          });
+        },
+      })
+    },
+  }),
+  withScriptjs
+)(props =>
+  <div className="location-search">
+    <StandaloneSearchBox
+      ref={props.onSearchBoxMounted}
+      bounds={props.bounds}
+      onPlacesChanged={props.onPlacesChanged}
+    >
+      <input placeholder="Choose location"/>
+    </StandaloneSearchBox>
+    <ol>
+      {props.places.map(({ place_id, formatted_address, geometry: { location } }) =>
+        <li key={place_id}>
+          {formatted_address}
+          {" at "}
+          ({location.lat()}, {location.lng()})
+        </li>
+      )}
+    </ol>
+  </div>
+);
+
+export default LocationSearch;
