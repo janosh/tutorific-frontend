9c9671a3937d2de2f20f0ccdc50d11ab0acfd7c9
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-25 13:05:56 +0200

    feat: add SupportPage

---
+++ src/pages/Support.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import './Support.css';
+
+export default class SupportPage extends React.Component {
+  render() {
+    return (
+      <div id="support-page">
+        <section id="faq">
+          <h1>Frequently asked questions</h1>
+          <h2>Commitment</h2>
+          <h3>I want to join you! What do I have to do?</h3>
+          <p>If you want to engage with us as a tutor, you should first contact the location management team in your city. The members of the leadership team will invite you to an introductory conversation and provide you with all the information you need.</p>
+          <h3>How long do I have to get involved with you at least?</h3>
+          <p>You should be able to get involved with us for at least one semester, so that the tuition students have a certain continuity. In addition, the recruitment and placement process takes some time. However, you are welcome to continue your engagement after graduation! In addition, there is the possibility for internships, stays abroad or similar. to pause your engagement.</p>
+          <h3>How much time should I bring for tutoring?</h3>
+          <p>In this regard, you can individually negotiate with your student. As a guideline, you can schedule about one hour of tutoring per week. However, it may happen that your student has less - in certain phases - e.g. during the holidays - or needs more care - e.g. before an exam. In addition, you should also allow some time for the approach and for the preparation of the tutoring.</p>
+          <h3>Do I need some experience in tutoring to get involved with you?</h3>
+          <p>Experiences in tutoring are certainly useful, but not a requirement for a commitment as a tutor in our club. In addition, we prepare you for your job as a tutor and support you with questions and problems.</p>
+          <h3>I'm not a student. Can I still give you tutoring?</h3>
+          <p>Yes and no: Although we only target students with our advertising measures, non-students can basically get involved with us. However, we would like to encourage especially young people to become involved with us, as our tutoring concept is based, among other things, on a small difference in age between tutors and students.</p>
+          <h3>What information and documents do you need from me?</h3>
+          <p>Certain precautions are essential in our work to protect the children and adolescents we support as much as possible. Therefore, before starting your engagement, you must submit filled out forms to us. In addition, we require an extended police clearance certificate from you, which you can apply for free at the registration office using a form. For more information on applying for the certificate of good conduct and the required documents, contact your site management team.</p>
+          <h2>Connecting students and tutors</h2>
+          <h3>Where do you get your coaching students from?</h3>
+          <p>To reach students in need of support, we work with various child and youth work organizations. These can be youth welfare offices, schools or children's homes, but also other clubs or church institutions. The type of partners varies from location to location. Students or families can also contact us directly.</p>
+          <h3>In which subjects can I tutor? Which grade / age / school types are you interested in?</h3>
+          <p>We have a variety of tutoring requests, so a variety of tutoring is required. Especially in the main subjects mathematics, German and English, the demand is usually very large. In terms of the age, the grade level and the school form of the student we do not restrict our offer of mediation. However, the need for follow-up varies from location to location. Of course you decide yourself in which subject and which grade you can and want to tutor.</p>
+          <h3>When and how can I get a tutor?</h3>
+          <p>Before starting your engagement, let us know your preferences regarding tutoring, such as: B. the desired school subjects and grades. As soon as we have received all the necessary documents from you, we are looking for a student who best suits your preferences. Then we establish contact with the appropriate institution or family to enable the beginning of the tutoring. How long it takes to find a suitable student for you depends in particular on your preferences and current need for follow-up. Depending on the constellation, the placement process may therefore take several weeks.</p>
+          <h2>Tutoring</h2>
+          <h3>Does the tutoring take place in individual or group lessons?</h3>
+          <p>We only teach private lessons. Since our goal is to support the students as intensively as possible and to practice learning strategies with them, we consider individual support to be the most appropriate type of instruction. This constellation is also designed to make it easier for the tutor to build a personal relationship with the tutor.</p>
+          <h3>How many students do I get assigned?</h3>
+          <p>Usually we only assign one student to you at the beginning. If, during the course of your engagement, you discover that you can and do care for other students, we are happy to provide you with additional students. However, this of course also means an increased expenditure of time.</p>
+          <h3>Where does the tutoring take place?</h3>
+          <p>The tutoring usually takes place in the premises of the social partner on whom the tutoring request reached us. In the case of missing premises or poor accessibility of the facility, we often resort to available rooms in public buildings (eg city library) or at the university. Your site management team can tell you more about the local conditions.</p>
+          <h3>Can the tuition take place at my home?</h3>
+          <p>No. For the protection of the student and for your own protection, the tutoring must under no circumstances take place at the teacher's home!</p>
+          <h3>Can I do other things than tutoring with my student?</h3>
+          <p>Of course we are happy when the relationship between student and teacher goes beyond the tutoring. Finally, joint ventures can strengthen one's personal relationship and expand the scope of the tutoring, eg. For example, cultural content in the form of a theater visit. It is important that such ventures be agreed in advance with all those responsible, i. both with the site manager and with the responsible supervisors or parents of the student. If necessary, costs incurred for this can be reimbursed by the association.</p>
+          <h3>The tutoring costs me (eg for teaching material or driving to set up). Can the club refund these costs?</h3>
+          <p>In principle, it is possible to reimburse costs incurred in tutoring. However, you should inquire in advance with your location team, if and which costs can be reimbursed.</p>
+          <h3>What if I want to do an internship or vacation during my engagement?</h3>
+          <p>That's not a problem. However, it is important that you inform both your protégé and his supervisor as well as your location team early if you need to pause the tutoring. Together, you can then decide whether to seek representation for the time of your absence.</p>
+          <h2>Troubleshooting</h2>
+          <h3>I have never tutored. Do you have any tips for me?</h3>
+          <p>You'll find some help and advice in a starter pack that we'll send you before your engagement. Although every student is different and speaks differently to different concepts accordingly. Do not let that put you off! We accompany you in your commitment and are always accessible. In addition, the informal site meetings provide an opportunity to interact with other tutors. So if you have questions or do not know how to deal with a problem, you are not on your own!</p>
+          <h3>If my student needs help in several subjects, do I have to do several separate lessons?</h3>
+          <p>That is up to you in consultation with the student and the supervisor. You can also teach different subjects in an hour, as long as it serves the success of the tutoring. If it makes sense to do several separate hours per week, but you do not have the time, ask your location team to see if another student can be transferred to the student. Then you can, for example, divide up the subjects.</p>
+          <h3>What can I do if I do not understand / feel comfortable with my tuition student / tutor?</h3>
+          <p>Here are several possibilities: If the student is just unmotivated, there may be ways to influence it. However, if the chemistry between you is fundamentally not correct, it is certainly better to convey yourself to another student. What is most sensible in your situation, you should find out together with your site manager. In any case, it is very important that you report to your location manager in a timely manner. To assist you with the tutoring is our task!</p>
+          <h3>My student does not play along properly. What can I do?</h3>
+          <p>There are many possible reasons why a student may be inattentive or unmotivated: Often, students are overwhelmed with the learning material, overlook their own advances, or lack the understanding of what they actually need the school supplies for. Helping students develop motivation for learning can be a great challenge for a tutor. The following train of thought should help you to strengthen the student's motivation.</p>
+          <p>What (life) goals and (future) plans does the student have? How can the subject matter or even learning itself help him achieve his goals?</p>
+          <p>Help him understand the purpose of his efforts!</p>
+          <p>Which learning goals are realistic for the student? How can the material be divided into digestible packages?</p>
+          <p>Set yourself or the student small intermediate goals that are accessible to him! This reduces demotivating setbacks.</p>
+          <p>What has the student already achieved?</p>
+          <p>Point out your achievements to your student and acknowledge his efforts!</p>
+          <p>Further help can be found in the starter pack for tutors. And as with all questions and problems, here as well we want to support you and that you can contact us at any time!</p>
+          <h2>Miscellaneous</h2>
+          <h3>How many private tutors are involved in the club?</h3>
+          <p>The number of tutors varies from location to location. Nationwide we currently have about 1000 active tutors.</p>
+          <h3>Since when does the club exist?</h3>
+          <p>The club was founded in July 2012. The individual locations are however different old. More about the club's history can be found under Club / History.</p>
+          <h3>What happens to my data?</h3>
+          <p>We used a database where we store information about our tutors and students' tutoring organization. Your written materials (such as your certificate of good conduct) will be kept for the duration of your engagement. Six months after the end of your engagement, all electronic data will be deleted. All site managers and federal boards are required to observe secrecy and to comply with the Data Protection Act. Your data will be used exclusively for association purposes and will not be passed on to third parties! If you have any questions about protecting your privacy, feel free to contact your location team or the Federal Executive.</p>
+          <h3>I would like to write applications soon. Can you give me a certificate of my commitment?</h3>
+          <p>Yes, we would like to issue you a certificate. Please contact your site management team regarding this.</p>
+        </section>
+        <section id="contact">
+          <h2>We're here to help!</h2>
+          <p>Your question is not in our FAQs? Then please reach out to us at <a href="mailto:support@tutorific.com">support@tutorific.com</a>. We'll get back to you within 24 hours.</p>
+        </section>
+      </div>
+    );
+  }
+}
