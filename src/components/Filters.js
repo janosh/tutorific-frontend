5839883266f67b95b05152690ae102a9110617ac
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-25 13:09:46 +0200

    feat: display additional (non-functional) filters in Filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -13,6 +13,44 @@ export default class Filters extends React.Component {
         <h2>Filters</h2>
         <Geosearch storePrefix="user"/>
         <SelectPersonType plural={true} storePrefix="filters"/>
+        <div>
+          <label htmlFor="status">Status</label>
+          <select
+            name="status"
+            id="status"
+            required
+            onChange={this.update}
+            // value={this.props.signUpData.status}
+          >
+            <option value="available">Available</option>
+            <option value="connected">Connected</option>
+            <option value="inactive">Inactive</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="last-login-after">Last login after</label>
+          <input
+            name="lastLoginAfter"
+            id="last-login-after"
+            type="date"
+            onChange={this.update}
+            // value={this.props.signUpData.birthday}
+          />
+        </div>
+        <div className="distance-filter">
+          <label htmlFor="distance">Distance</label>
+          <div name="distance" className="distance-buttons">
+            <button className="selected">
+              5 km
+            </button>
+            <button>
+              10 km
+            </button>
+            <button>
+              20 km
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
