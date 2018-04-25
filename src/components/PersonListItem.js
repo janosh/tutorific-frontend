f45221a5f668e5c635e6e192957c6b95e72ab268
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-25 13:03:20 +0200

    feat: add address, gender, last login to PersonListItem, remove email

--- src/components/PersonListItem.js
+++ src/components/PersonListItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import moment from 'moment';
+
 import './PersonListItem.css';
 
 export default function PersonListItem({person}) {
   return (
     <div className="person-list-item">
       <h3>{person.firstname} {person.lastname}</h3>
-      <p className="status">status: {person.status}</p>
-      <p className="status">email: {person.email}</p>
-      <p className="status">subjects: {person.subjects.map(subject => subject.name).join(', ')}</p>
+      <p><span>Status</span> {person.status}</p>
+      <p><span>Subjects</span> {person.subjects.map(subject => subject.name).join(', ')}</p>
+      <p><span>Address</span> {person.address.city}</p>
+      <p><span>Gender</span> {person.gender}</p>
+      <p><span>Last login</span> {moment(person.updatedAt).format('MMMM DD, YYYY, h:mm:ss')}</p>
     </div>
   );
 }
