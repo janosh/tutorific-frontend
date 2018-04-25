e9c5de6e5c1cb62c3c6a84a31924441da382454c
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-25 13:08:16 +0200

    style: add hero background image to LandingPage, move logo to public directory

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from "react-router-dom";
 
-import logo from '../assets/logo.svg';
 import {setUserLocation} from '../actions';
 import './Landing.css';
 
@@ -11,8 +10,10 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div id="landing-page">
-        <img src={logo} alt="Logo"/>
-        <h1><Link to="/find">Get free tutoring from a student near you!</Link></h1>
+        <div className="overlay">
+          <img src="./assets/logo.svg" alt="Logo"/>
+          <h1><Link to="/find">Get free tutoring from a student near you!</Link></h1>
+        </div>
       </div>
     );
   }
