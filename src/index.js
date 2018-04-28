eedd372cd21417833a377460971130af0d5d4fcb
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-28 19:30:37 +0200

    refactor: outsource redux store configuration into separate file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from './reducers';
 
 import './index.css';
 import App from './App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
-
-let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
