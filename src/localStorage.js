d0cea0e46b89e57e1e07053aa8b6431986c9612c
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-28 19:31:06 +0200

    feat: implement store persistence to browser local storage

---
+++ src/localStorage.js
@@ -0,0 +1,18 @@
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (!serializedState) return;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return;
+  }
+}
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    console.error(err);
+  }
+}
