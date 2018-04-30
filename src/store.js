7fc2abcd44c4f1d82aec192fab77a877e1892968
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-30 09:46:47 +0200

    chore: temporarily disbale persisting reduxstore to local storage

--- src/store.js
+++ src/store.js
@@ -3,13 +3,13 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import reducers from './reducers';
 import thunk from './middlewares/thunk';
 import backendCall from './middlewares/backendCall';
-import {loadState, saveState} from './localStorage';
+// import {loadState, saveState} from './localStorage';
 
-const persistedState = loadState();
+// const persistedState = loadState();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, /*persistedState,*/ composeEnhancers(applyMiddleware(thunk, backendCall)));
 
-store.subscribe(() => saveState(store.getState()));
+// store.subscribe(() => saveState(store.getState()));
 
 export default store;
