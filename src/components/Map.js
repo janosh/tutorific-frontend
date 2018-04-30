22cb659481594544b4857d94b6ec46e25c41edf5
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-04-30 12:34:40 +0200

    feat: show students and tutors as markers on Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,14 +8,25 @@ import './Map.css';
 class Map extends React.Component {
 
   render() {
-    const {lng: userLng, lat: userLat} = this.props.userLocationChoice.location || this.props.userLocation;
+    const {userLocation, userLocationChoice, list} = this.props;
+    const {lng: userLng, lat: userLat} = (userLocationChoice && userLocationChoice.location) || userLocation;
     return (
       <div className="map-container">
         <GoogleMapReact
+          className="map"
           bootstrapURLKeys={{key: config.googleMapsApiKey}}
           center={{lat: userLat || 50, lng: userLng || 10}}
           zoom={userLng ? 12 : 4}
         >
+          {list && list.map(person =>
+            <div
+              className="marker"
+              lat={person.address.location.lat}
+              lng={person.address.location.lng}
+            >
+              {person.firstname}
+            </div>
+          )}
         </GoogleMapReact>
       </div>
     );
@@ -25,7 +36,7 @@ class Map extends React.Component {
 const mapStateToProps = (state) => {
   return {
     userLocation: state.currentUser.location,
-    userLocationChoice: state.currentUser.locationChoice,
+    userLocationChoice: state.filters.address,
   };
 };
 
