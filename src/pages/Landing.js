68e9d381d7239af04f0ff35405b6e654dd09bf44
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-01 17:33:17 +0200

    feat: add Geosearch to LandingPage and redirect to ConnectPage onSuggestSelect

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
+import Geosearch from '../components/Geosearch';
 
 import SelectPersonType from '../components/SelectPersonType';
 import './Landing.css';
@@ -13,6 +14,7 @@ class LandingPage extends React.Component {
         <div className="overlay">
           <img src="./assets/logo.svg" alt="Logo"/>
           <SelectPersonType storePrefix="signup"/>
+          <Geosearch storePrefix="filters"/>
           {this.props.userType === 'student' ?
           <h1><Link to="/connect">Get free tutoring from a student near you!</Link></h1> :
           <h1><Link to="/signup">Sign up as a tutor today and make a difference in a child's life!</Link></h1>}
