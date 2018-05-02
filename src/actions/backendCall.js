c96a6e23f62f01e83b9818ea39a86e0f3cb60cda
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-02 10:20:31 +0200

    feat: comply with new person controller endpoints of updated backend

--- src/actions/backendCall.js
+++ src/actions/backendCall.js
@@ -2,14 +2,14 @@ export const getPersonList = (params) => ({
   type: 'get_person_list',
   params,
   backendCall: {
-    endpoint: params.userType + 's',
+    endpoint: 'persons',
   }
 });
 
 export const submitSignupForm = (data) => ({
   type: 'submit_signup_form',
   backendCall: {
-    endpoint: data.userType,
+    endpoint: 'person',
     method: 'POST',
     body: data,
   }
