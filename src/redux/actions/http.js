7e41f0ed5a1f6bda7d6e44a7e5b4cb102ea7f38e
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-21 13:23:04 +0200

    chore: rename backendCall redux middleware to http

--- src/redux/actions/http.js
+++ src/redux/actions/http.js
@@ -1,14 +1,14 @@
 export const getPersonList = (params) => ({
   type: 'get_person_list',
   params,
-  backendCall: {
+  http: {
     endpoint: 'persons',
   }
 });
 
 export const submitSignupForm = (data) => ({
   type: 'submit_signup_form',
-  backendCall: {
+  http: {
     endpoint: 'person',
     method: 'POST',
     body: data,
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,6 @@
 export * from './app';
 export * from './login';
-export * from './backendCall';
+export * from './http';
 export * from './filters';
 export * from './signup';
 export * from './location';
