1dcc8142a4aab2191bc7449ae4a0f5855799e528
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-21 15:34:12 +0200

    feat: outsource Marker css into separate file

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Marker.css'
+
 const Marker = ({ text, person, setSinglePersonView }) => (
   <div onClick={() => setSinglePersonView(person)} className="marker">
     {text}
