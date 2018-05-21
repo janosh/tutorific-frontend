a483582f9276b3aa958323dcf4cefa5d3e3662db
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-21 13:23:31 +0200

    feat: make PageNotFound stateless

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -2,16 +2,11 @@ import React from 'react';
 
 import './PageNotFound.css';
 
-class PageNotFound extends React.Component {
-
-  render() {
-    return (
-      <div id="page-not-found">
-        <h1>You ran into a 404!</h1>
-        <h2>The reqested page does not exist.</h2>
-      </div>
-    );
-  }
-}
+const PageNotFound = () => (
+  <div id="page-not-found">
+    <h1>404</h1>
+    <h2>The reqested page does not exist.</h2>
+  </div>
+);
 
 export default PageNotFound;
