d25978886e8957ad94689eff5c64abfee6b10cbc
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-21 13:24:02 +0200

    feat: make PersonListItem stateless

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -3,17 +3,24 @@ import {connect} from 'react-redux';
 
 import PersonListItem from './PersonListItem';
 import './PersonList.css';
+import {setSinglePersonView} from '../redux/actions';
 
 class PersonList extends React.Component {
 
   render() {
-    const {personList, filters} = this.props;
+    const {personList, filters, setSinglePersonView} = this.props;
     return (
-      personList.length === 0 ?
-      <div className="empty-list"><h2>No {filters.userType}s to display.</h2></div> :
-      <div className="person-list">
+      personList.length === 0
+      ? <div className="empty-list">
+        <h2>No {filters.userType}s to display.</h2>
+      </div>
+      : <div className="person-list">
         {personList.map(person =>
-          <PersonListItem key={person._id} person={person}/>
+          <PersonListItem
+            key={person._id}
+            person={person}
+            setSinglePersonView={() => setSinglePersonView(person)}
+          />
         )}
       </div>
     );
@@ -27,4 +34,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(PersonList);
+const mapDispatchToProps = (dispatch) => ({
+  setSinglePersonView: (person) => dispatch(setSinglePersonView(person)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PersonList);
