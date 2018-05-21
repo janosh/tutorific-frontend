cdfc9f953a190c4471f355c925842627564c29c3
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-21 13:24:02 +0200

    feat: make PersonListItem stateless

--- src/components/PersonListItem.js
+++ src/components/PersonListItem.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import moment from 'moment';
 
-import {setSinglePersonView} from '../redux/actions';
 import './PersonListItem.css';
 
-class PersonListItem extends React.Component {
-  render() {
-    const person = this.props.person;
-    return (
-      <div className="person-list-item" onClick={() => this.props.setSinglePersonView(person)}>
-        <h3>{person.firstName} {person.lastName}</h3>
-        <p><span>Status</span> {person.status}</p>
-        <p><span>Subjects</span> {person.subjects.join(', ')}</p>
-        {person.location && person.location.label && <p><span>Address</span> {person.location.label}</p>}
-        <p><span>Gender</span> {person.gender}</p>
-        <p><span>Last login</span> {moment(person.updatedAt).format('MMMM DD, YYYY')}</p>
-      </div>
-    );
-  }
-}
+const PersonListItem = ({person, setSinglePersonView}) => (
+  <div className="person-list-item" onClick={setSinglePersonView}>
+    <h3>{person.firstName} {person.lastName}</h3>
+    <p><span>Status</span> {person.status}</p>
+    <p><span>Subjects</span> {person.subjects.join(', ')}</p>
+    {person.location && person.location.label && <p><span>Address</span> {person.location.label}</p>}
+    <p><span>Gender</span> {person.gender}</p>
+    <p><span>Last login</span> {moment(person.updatedAt).format('MMMM DD, YYYY')}</p>
+  </div>
+);
 
-const mapDispatchToProps = (dispatch) => ({
-  setSinglePersonView: (person) => dispatch(setSinglePersonView(person)),
-});
-
-export default connect(null, mapDispatchToProps)(PersonListItem);
+export default PersonListItem;
