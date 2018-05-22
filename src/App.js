77d7ca92aff2bf24042917871348a7b1c1124a18
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-22 23:19:26 +0200

    feat: remove old sign up page from App router

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import LandingPage from './pages/Landing';
 import ConnectPage from './pages/Connect';
 import AboutPage from './pages/About';
 import SupportPage from './pages/Support';
-import SignupPage from './pages/Signup';
-import SignupForm from './pages/ReduxForm';
+import SignupForm from './pages/Signup';
 import PageNotFoundPage from './pages/PageNotFound';
 import Login from './components/Login';
 
@@ -30,7 +29,6 @@ export default class App extends React.Component {
                 </div>
               </div>
               <div id="signup-nav">
-                <Link to="/signupform">Sign up form</Link>
                 <Link to="/signup">Sign up</Link>
                 <Link to="#"><Login/></Link>
               </div>
@@ -42,8 +40,7 @@ export default class App extends React.Component {
               <Route path="/connect" component={ConnectPage}/>
               <Route path="/about" component={AboutPage}/>
               <Route path="/support" component={SupportPage}/>
-              <Route path="/signup" component={SignupPage}/>
-              <Route path="/signupform" component={SignupForm}/>
+              <Route path="/signup" component={SignupForm}/>
               <Route path="/*" component={PageNotFoundPage}/>
             </Switch>
           </main>
