416b58c147491f0ed126d17b3fd6a723778f45f1
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-22 23:20:43 +0200

    chore: rename new redux form sign up according to old sign up

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,36 +1,12 @@
 import React from 'react';
 import {Field, FieldArray, reduxForm} from 'redux-form';
-import validator from 'validator';
 import Geosuggest from 'react-geosuggest';
 
-import './ReduxForm.css'
-
-const validate = (values) => {
-  const errors = {};
-  if (!values.firstName) errors.firstName = 'Required';
-  if (!values.lastName) errors.lastName = 'Required';
-  if (!values.email) errors.email = 'Required';
-  else if (!validator.isEmail(values.email)) errors.email = 'Invalid email';
-  if (values.phone && !validator.isMobilePhone(values.phone, 'any', {strictMode: true})) errors.phone = 'Invalid: Supply full country code, e.g. +49 for Germany followed by a space';
-  if (!values.password) errors.password = 'Required';
-  else if (values.password.length < 6) errors.password = 'Password must be at least 6 characters long';
-  else if (values.password.search(/[a-z]/) < 0) errors.password = 'Password must contain a lowercase character';
-  else if(values.password.search(/[A-Z]/) < 0) errors.password = 'Password must contain an uppercase character';
-  else if (values.password.search(/[0-9]/) < 0) errors.password = 'Password must contain a number';
-  if (!values.confirmPassword) errors.confirmPassword = 'Required';
-  else if (values.password !== values.confirmPassword) errors.confirmPassword = 'Passwords do not match';
-  if (!values.address) errors.address = 'Required';
-  else if (!values.address.label) errors.address = 'Please make a selection from the address suggestions';
-  if (!values.gender) errors.gender = 'Required';
-  if (!values.subjects || !values.subjects.length) {
-    errors.subjects = {_error: 'Enter at least one subject'}
-  } else {
-    const subjectErrors = values.subjects.map(el => (subjects.includes(el) || el === '') ? '' : 'Invalid subject')
-    if(subjectErrors.reduce((a,b) => a + b, '') !== '') errors.subjects = subjectErrors;
-  }
-  console.log(errors.subjects);
-  return errors;
-}
+import validate, {subjects} from './validate';
+import asyncValidate from './asyncValidate';
+import prefill from './prefill';
+import {submitSignupForm} from '../../redux/actions';
+import './Signup.css';
 
 const handleSelection = (input) => (select) => {
   if (!select || !select.location) return;
@@ -42,22 +18,23 @@ const handleSelection = (input) => (select) => {
   })
 }
 
-const GeosuggestComp = ({input, label, ...rest}) => (
-  <div>
+const GeosuggestComp = (props) => {
+  const {input, label, ...rest} = props;
+  return <div>
     <label htmlFor={input.name}>{label}</label>
     <Geosuggest
+      id={input.name} {...rest}
       initialValue={input.value.label}
       onSuggestSelect={handleSelection(input)}
-      id={input.name} {...rest}
     />
   </div>
-)
+}
 
 const inputComp = ({input, label, meta: {touched, error, warning}, ...rest}) => {
   return <div>
     <label htmlFor={input.name}>{label}</label>
-    {error && touched && <span className="error">{error}</span>}
     <input id={input.name} {...input} {...rest}/>
+    {error && touched && <span className="error">{error}</span>}
   </div>
 }
 
@@ -79,13 +56,12 @@ const inRange = (min, max) => (val) => {
   return (val < min && min) || (val > max && max) || val;
 };
 
-const subjects = ['Math', 'Physics', 'Chemistry', 'Biology', 'Computer Science',
-  'English', 'Spanish', 'French', 'German', 'Italian', 'Chinese',
-  'Japanese', 'Latin', 'History', 'Politics', 'Ethics', 'Social Studies',
-  'Philosophy', 'Economics', 'Physical Education', 'Music', 'Art'];
+const submitForm = (values, dispatch) => {
+  dispatch(submitSignupForm(values));
+}
 
 const SignupForm = ({handleSubmit, invalid, submitting, pristine, reset}) => {
-  return <form id="redux-form" onSubmit={handleSubmit}>
+  return <form id="redux-form" onSubmit={handleSubmit(submitForm)}>
     <datalist id="subjects">
       {subjects.map(el => <option key={el}>{el}</option>)}
     </datalist>
@@ -102,15 +78,22 @@ const SignupForm = ({handleSubmit, invalid, submitting, pristine, reset}) => {
     <Field name="maxStudentGrade" component={inputComp} label="Maximum Student Grade" placeholder="13" normalize={inRange(0, 13)}/>
     <FieldArray name="subjects" component={inputComps} label="Subject" list="subjects" placeholder={subjects}/>
     <h2>Personal</h2>
-    <Field name="address" component={GeosuggestComp} label="Address" placeholder="Infinite Loop 1, Cupertino"/>
+    <Field name="location" component={GeosuggestComp} label="Address" placeholder="Infinite Loop 1, Cupertino"/>
     <Field name="birthDate" component={inputComp} label="Date of Birth" type="date"/>
     <Field name="birthPlace" component={inputComp} label="Place of Birth" placeholder="Paradise City"/>
-    <Field name="semester" component={inputComp} label="Semester" placeholder="3" normalize={[inRange(0, 50), inRange(0, 40)]}/>
-    <Field name="fieldOfStufy" component={inputComp} label="Field of Study" placeholder="Physics"/>
+    <Field name="semester" component={inputComp} label="Semester" placeholder="3" normalize={inRange(0, 50)}/>
+    <Field name="fieldOfStudy" component={inputComp} label="Field of Study" placeholder="Physics"/>
     <div className="form-end"></div>
     <button type="submit" disabled={invalid || submitting}>Submit</button>
     <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
   </form>
 }
 
-export default reduxForm({form: 'signup', destroyOnUnmount: false, validate})(SignupForm);
+export default reduxForm({
+  form: 'signup',
+  destroyOnUnmount: false,
+  validate,
+  initialValues: prefill,
+  asyncValidate,
+  asyncBlurFields: ['email'],
+})(SignupForm);
