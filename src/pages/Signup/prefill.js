22de86de7502b7bb529c05fce6159cb948a28d0b
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-22 23:21:59 +0200

    test: add initial values to prefill sign up form for faster testing

---
+++ src/pages/Signup/prefill.js
@@ -0,0 +1,32 @@
+export default {
+  // account info
+  firstName: 'John',
+  lastName: 'Doe',
+  password: 'yxcvbnm',
+  confirmPassword: 'yxcvbnm',
+  email: 'john@doe.com',
+  userType: 'tutor',
+  // contact info
+  phone: '+49 151 1234 5678',
+  location: {
+    label: 'Heidelberg, Germany',
+    placeId: 'ChIJzdzMDgXBl0cR1zokRADq5u8',
+    lat: 49.3987524,
+    lng: 8.672433500000011,
+  },
+  // personal info
+  birthDate: '1990-03-24',
+  birthPlace: 'Hamburg, Germany',
+  gender: 'male',
+  // student-specific data
+  youthOrganization: 'Unicef',
+  grade: '7',
+  schoolType: 'Middle',
+  // tutor-specific data
+  semester: '5',
+  fieldOfStudy: 'Theoretical Physics',
+  minStudentGrade: '5',
+  maxStudentGrade: '12',
+  // subjects info
+  subjects: ['Physics', 'Math', 'English', 'Biology'],
+};
