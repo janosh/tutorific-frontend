aabb05b2deccdc85a2d5505e86bee604036cdb09
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-26 23:06:21 +0200

    fix: handle typeof action.data = number if dbQuery cmd=count in personList reducer

--- src/redux/reducers/personList.js
+++ src/redux/reducers/personList.js
@@ -3,7 +3,7 @@
 import initialState from './initialState';
 
 export const personList = (state = initialState.personList, action) => {
-  if (action.type === 'get_person_list_success' && action.data)
+  if (action.type === 'get_person_list_success' && action.data && Array.isArray(action.data))
     return action.data;
   return state;
 };
