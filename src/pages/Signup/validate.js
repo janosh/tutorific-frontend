a1b768cf20125be8a99593db7e3c8b1ca1dd3e3e
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-26 22:52:34 +0200

    feat: extensive rewrite of Signup

--- src/pages/Signup/validate.js
+++ src/pages/Signup/validate.js
@@ -1,17 +1,26 @@
 import validator from 'validator';
 
-export const subjects = ['Math', 'Physics', 'Chemistry', 'Biology', 'Computer Science',
+export const subjects = [
+  'Math', 'Physics', 'Chemistry', 'Biology', 'Computer Science',
   'English', 'Spanish', 'French', 'German', 'Italian', 'Chinese',
   'Japanese', 'Latin', 'History', 'Politics', 'Ethics', 'Social Studies',
-  'Philosophy', 'Economics', 'Physical Education', 'Music', 'Art'];
+  'Philosophy', 'Economics', 'Physical Education', 'Music', 'Art'
+];
+
+export const schools = [
+  'Elementary', 'Middle', 'High', 'Special Needs', 'Vocational', 'Refugee'
+];
 
 const validate = (values) => {
-  const errors = {};
+  const errors = {address: {}};
+  // account
   if (!values.firstName) errors.firstName = 'Required';
   if (!values.lastName) errors.lastName = 'Required';
   if (!values.email) errors.email = 'Required';
   else if (!validator.isEmail(values.email)) errors.email = 'Invalid email';
-  if (values.phone && !validator.isMobilePhone(values.phone, 'any', {strictMode: true})) errors.phone = 'Invalid: Supply full country code, e.g. +49 for Germany followed by a space';
+  if (values.phone && !validator.isMobilePhone(values.phone, 'any', {strictMode: true}))
+    errors.phone = 'Invalid: Supply full country code, e.g. +49 for Germany followed by a space';
+  // password
   if (!values.password) errors.password = 'Required';
   else if (values.password.length < 6) errors.password = 'Password must be at least 6 characters long';
   else if (values.password.search(/[a-z]/) < 0) errors.password = 'Password must contain a lowercase character';
@@ -19,8 +28,13 @@ const validate = (values) => {
   else if (values.password.search(/[0-9]/) < 0) errors.password = 'Password must contain a number';
   if (!values.confirmPassword) errors.confirmPassword = 'Required';
   else if (values.password !== values.confirmPassword) errors.confirmPassword = 'Passwords do not match';
-  if (!values.address) errors.address = 'Required';
-  else if (!values.address.label) errors.address = 'Please make a selection from the address suggestions';
+  // address
+  if (values.address) {
+    if (!values.address.street) errors.address.street = 'Required';
+    if (!values.address.zipCode) errors.address.zipCode = 'Required';
+    if (!values.address.city) errors.address.city = 'Required';
+  }
+  // personal
   if (!values.gender) errors.gender = 'Required';
   if (!values.subjects || !values.subjects.length) {
     errors.subjects = {_error: 'Enter at least one subject'}
